67322274352ae19412235ccb7c90b2f051808865
jhasuraj01 <44930179+jhasuraj01@users.noreply.github.com>
2022-04-23 17:38:41 +0000

    implemented rotate left and rotate right

--- src/AVL.ts
+++ src/AVL.ts
@@ -1,8 +1,58 @@
 import { BST } from "./index";
+import type { Node } from './BST';
 
 class AVL<K, V> extends BST<K, V> {
     constructor() {
         super();
+    }
+
+    private rotateRight(node: Node<K, V>): void {
+        const parent = node.parent;
+        const left = node.left;
+        const lRight = node.left?.right;
+
+        if(left) {
+            left.parent = parent;
+            left.right = node;
+            node.parent = left;
+            node.left = lRight ? lRight : null;
+        }
+    }
+
+    private rotateLeft(node: Node<K, V>): void {
+        const parent = node.parent;
+        const right = node.right;
+        const rLeft = node.right?.left;
+
+        if(right) {
+            right.parent = parent;
+            right.left = node;
+            node.parent = right;
+            node.right = rLeft ? rLeft : null;
+        }
+    }
+
+    private rebalance(node: Node<K, V>): void {
+        const parent = node.parent;
+        if(parent == null) return;
+    }
+
+    public insert(key: K, value: V): Node<K, V> {
+        const node = super.insert(key, value);
+        this.rebalance(node);
+        return node;
+    }
+
+    public delete(key: K): void {
+        const ref = this.find(key);
+        if (ref == null || ref.key != key) {
+            // tree empty or element not found
+            return;
+        }
+        this.deleteElement(ref);
+        const parentOfReplacingElement = this.insertionPoint(key);
+        if(parentOfReplacingElement)
+            this.rebalance(parentOfReplacingElement);
     }
 }
 
